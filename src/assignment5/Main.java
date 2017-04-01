fd04367e1ea3ea408d32600445a7af31cba34f5f
raiyanac <raiyan.chowdhury@utexas.edu>
2017-03-31 22:07:41 -0500

    cleaned up main, added javadoc style comments

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -48,7 +48,18 @@ public class Main extends Application{
 		launch(args);
 	}
 
-	
+	/**
+	 * This is what ultimately launches the Critter GUI.
+	 * A GridPane for the controller component and a Group for displaying the world
+	 * are created here. 
+	 * The controller component consists of the options to set a number of timesteps 
+	 * to execute, set the seed of the random number generator used by Critter, create
+	 * any number of critters desired at will, have time steps occur automatically via 
+	 * animation at varying speeds, and have stats displayed for any critter the user wants.
+	 * Every time the world is changed in some way -- if critters are added or if a time step
+	 * occurs -- Critter.displayWorld() is called and the world view is updated.
+	 * @param primaryStage
+	 */
 	@Override
 	public void start(Stage primaryStage) {
 		//code for getting class names
@@ -104,10 +115,6 @@ public class Main extends Application{
 		gridPane.add(errorMsg, 0, 10);
 		
 		// input for critter type, button to display stats, box for displaying the stats
-		/*TextField critterInput = new TextField();
-		critterInput.setPromptText("Critter type");
-		critterInput.setPrefWidth(350);
-		gridPane.add(critterInput, 0, 4);*/
 		//text for displaying stats
 		Text stats = new Text();
 		stats.setWrappingWidth(360);
@@ -160,38 +167,6 @@ public class Main extends Application{
 		}
 		critterInput.getItems().addAll(class_items);
 		gridPane.add(critterInput, 0, 4);
-				
-		/*Button runStats = new Button();
-		runStats.setText("Get Statistics");
-		gridPane.add(runStats, 2, 4);
-		runStats.setOnAction(new EventHandler<ActionEvent>() {
-			@Override public void handle(ActionEvent e) {
-				try {
-					String critter_stats = "";
-					for (String critter_class_name : display_or_not.keySet()) {
-						if (display_or_not.get(critter_class_name) == true) {
-							List<Critter> list_of_instances = Critter.getInstances(critter_class_name);
-							String critter_class = myPackage + "." + critter_class_name;
-							Class<?> type = Class.forName(critter_class);
-							Method method = type.getMethod("runStats", List.class);
-							critter_stats = critter_stats + critter_class_name + " stats:\n";
-							critter_stats = critter_stats + (String) method.invoke(null, list_of_instances) + "\n\n";
-						}
-					}
-					if (critter_stats.equals("")) {
-						stats.setText("");
-						errorMsg.setText("Please choose at least one critter type.");
-					}
-					else {
-						stats.setText(critter_stats);
-						errorMsg.setText("");
-					}
-			    } catch (Exception f){
-			    	errorMsg.setText("You did not enter a known critter type for stats!");
-			    	stats.setText("");
-			    }
-			}
-		});*/
 		
 		// Time Step button and text box functionality
 		TextField stepInput = new TextField();
@@ -258,10 +233,7 @@ public class Main extends Application{
 		});
 		
 		// Code to create critters. Includes text box for critter type, amount desired, and button to activate.
-				/*TextField critterInput2 = new TextField();
-				critterInput2.setPromptText("Enter desired critter type");
-				critterInput2.setPrefWidth(350);
-				gridPane.add(critterInput2, 0, 2);*/
+				
 				final ComboBox<String> critterInput2 = new ComboBox<String>();
  				critterInput2.getItems().addAll(classes);
  				critterInput2.setPrefWidth(350);
@@ -338,9 +310,6 @@ public class Main extends Application{
             	Critter.displayWorld(pane);
             }
         };
-        /*TextField animationSpeed = new TextField();
-        animationSpeed.setPromptText("Enter the desired speed of animation");
-        animationSpeed.setPrefWidth(350);*/
         Slider animationSpeed = new Slider();
         animationSpeed.setMin(0);
         animationSpeed.setMax(100);
@@ -360,7 +329,6 @@ public class Main extends Application{
 		    		createCritters.setDisable(false);
 		    		amtToSpawn.setDisable(false);
 		    		critterInput2.setDisable(false);
-		    		//runStats.setDisable(false);
 		    		critterInput.setDisable(false);
 		    		stats.setDisable(false);
 		    		seedSet.setDisable(false);
@@ -375,7 +343,6 @@ public class Main extends Application{
 		    		createCritters.setDisable(true);
 		    		amtToSpawn.setDisable(true);
 		    		critterInput2.setDisable(true);
-		    		//runStats.setDisable(true);
 		    		critterInput.setDisable(true);
 		    		stats.setDisable(true);
 		    		seedSet.setDisable(true);
@@ -396,216 +363,4 @@ public class Main extends Application{
 }
 
 
-/*OLD MAIN
 
-
-/*
- * Usage: java <pkgname>.Main <input file> test
- * input file is optional.  If input file is specified, the word 'test' is optional.
- * May not use 'test' argument without specifying input file.
- */
- /*
-public class Main {
-
-    static Scanner kb;	// scanner connected to keyboard input, or input file
-    private static String inputFile;	// input file, used instead of keyboard input if specified
-    static ByteArrayOutputStream testOutputString;	// if test specified, holds all console output
-    private static String myPackage;	// package of Critter file.  Critter cannot be in default pkg.
-    private static boolean DEBUG = false; // Use it or not, as you wish!
-    static PrintStream old = System.out;	// if you want to restore output to console
-
-
-    // Gets the package name.  The usage assumes that Critter and its subclasses are all in the same package.
-    static {
-        myPackage = Critter.class.getPackage().toString().split(" ")[1];
-    }
-
-    /**
-     * Main method.
-     * @param args args can be empty.  If not empty, provide two parameters -- the first is a file name, 
-     * and the second is test (for test output, where all output to be directed to a String), or nothing.
-     *//*
-    public static void main(String[] args) { 
-        if (args.length != 0) {
-            try {
-                inputFile = args[0];
-                kb = new Scanner(new File(inputFile));			
-            } catch (FileNotFoundException e) {
-                System.out.println("USAGE: java Main OR java Main <input file> <test output>");
-                e.printStackTrace();
-            } catch (NullPointerException e) {
-                System.out.println("USAGE: java Main OR java Main <input file>  <test output>");
-            }
-            if (args.length >= 2) {
-                if (args[1].equals("test")) { // if the word "test" is the second argument to java
-                    // Create a stream to hold the output
-                    testOutputString = new ByteArrayOutputStream();
-                    PrintStream ps = new PrintStream(testOutputString);
-                    // Save the old System.out.
-                    old = System.out;
-                    // Tell Java to use the special stream; all console output will be redirected here from now
-                    System.setOut(ps);
-                }
-            }
-        } else { // if no arguments to main
-            kb = new Scanner(System.in); // use keyboard and console
-        }
-
-        /* Do not alter the code above for your submission. */
-        /* Write your code below. */
-        /*
-        boolean continueSim= true;
-        while(continueSim) {
-        	ArrayList<String> tokens = parse(kb);
-        	if(tokens.size() > 0) {
-	        	try {
-	        		continueSim = execute(tokens);
-	        	} catch (Exception e) {
-	        		System.out.print("error processing:");
-	        		for(String token : tokens) {
-	        			System.out.print(" " + token);
-	        		}
-	        		System.out.println();
-	        	}
-        	}
-        }
-        
-        /* Write your code above *//*
-        System.out.flush();
-
-    }
-    
-    /**
-     * Converts the input into an array of String tokens. 
-     * @param kb - keyboard input
-     * @return - array of tokens relatig to input
-     *//*
-    public static ArrayList<String> parse(Scanner kb) {
-    	String token = kb.nextLine();
-    	Scanner sc = new Scanner(token);
-    	ArrayList<String> tokens = new ArrayList<String>();
-    	while(sc.hasNext()) {
-    		tokens.add(sc.next());
-    	}
-    	sc.close();
-    	return tokens;
-    }
-    
-    /**
-     * Throws an exception if the command is not the correct length of tokens.
-     * @param tokens - keyboard input
-     * @param length - desired length of command
-     * @throws Exception - thrown if not desired length
-     *//*
-    public static void checkCommandLength(ArrayList<String> tokens, int length) throws Exception {
-    	if(tokens.size() != length) throw new Exception();
-    }
-    
-    /**
-     * Takes a keyboard input and processes it. Checks for each command or 
-     * prints an error if no correct command exists. prints a different error 
-     * if the command is not structured correctly. 
-     * @param tokens - keyboard input structured as a series of tokens
-     * @return - whether or not to continue checking for commands, in other 
-     * words if "quit" was input.
-     * @throws Exception - thrown if incorrect command structure.
-     *//*
-    public static boolean execute(ArrayList<String> tokens) throws Exception {
-    	// quit
-    	if(tokens.get(0).equals("quit")) {
-    		try {
-    			checkCommandLength(tokens, 1);
-    			return false;
-    		} catch (Exception e){
-    			throw e;
-    		}
-    	}
-    	// show
-    	if(tokens.get(0).equals("show")) {
-    		try {
-    			checkCommandLength(tokens, 1);
-    			Critter.displayWorld();
-    		} catch (Exception e){
-    			throw e;
-    		}
-    		return true;
-    	}
-    	// step
-    	if(tokens.get(0).equals("step")) {
-    		int steps = 1;
-    		if(tokens.size() > 1) {
-    			try {
-    				checkCommandLength(tokens, 2);
-    				steps = Integer.parseInt(tokens.get(1));
-    			} catch (Exception e){ // next token isnt an int
-    				throw e;
-    			}
-    		}
-    		while (steps > 0) {
-    			Critter.worldTimeStep();
-    			steps--;
-    		}
-    		return true;
-    	}
-    	// seed
-    	if(tokens.get(0).equals("seed")) {
-    		try {
-    			checkCommandLength(tokens, 2);
-    			Critter.setSeed(Integer.parseInt(tokens.get(1)));
-    		} catch (Exception e){ // next token isnt an int
-				throw e;
-			}
-    		return true;
-    	}
-    	// make
-    	if(tokens.get(0).equals("make")) {
-    		// checks if number specified, or if not then 1
-    		int num = 1;
-    		if(tokens.size() > 2) {
-    			try {
-    				checkCommandLength(tokens, 3);
-    				num = Integer.parseInt(tokens.get(2));
-    			} catch (Exception e){ // next token isn't an int or too many tokens
-    				throw e;
-    			}
-    		}
-    		try {
-    			String critter_class_name = tokens.get(1);
-    			for (int i = 0; i < num; i++) {
-    				Critter.makeCritter(critter_class_name);
-    			}
-    		} catch (Exception e) {
-    			throw e;
-    		}
-    		
-    		return true;
-    	}
-    	// stats
-    	if(tokens.get(0).equals("stats")) {
-    		try {
-    			checkCommandLength(tokens, 2);
-    			String critter_class_name = tokens.get(1);
-    			if (critter_class_name.toLowerCase().equals("critter"))
-    				throw new Exception();
-    			List<Critter> list_of_instances = Critter.getInstances(critter_class_name);
-    			critter_class_name = myPackage + "." + critter_class_name;
-    			Class<?> type = Class.forName(critter_class_name);
-    			Method method = type.getMethod("runStats", List.class);
-    			method.invoke(null, list_of_instances);
-    		}
-    		catch (Exception e) {
-    			throw e;
-    		}
-    		return true;
-    	}
-    	//unknown command
-    	System.out.print("invalid command:");
-		for(String token : tokens) {
-			System.out.print(" " + token);
-		}
-		System.out.println();
-		return true;
-		
-    }
-}
-*/
